31410c2471ac7603c27fad34419929e519b98f10
nori8774 <nori8774@gmail.com>
2025-12-14 10:35:18 +0900

    Initial Commit

---
+++ PDF_Reader/PDF_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_ReaderApp.swift
+//  PDF_Reader
+//
+//  Created by Norimasa Yamamoto on 2025/12/14.
+//
+
+import SwiftUI
+
+@main
+struct PDF_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
